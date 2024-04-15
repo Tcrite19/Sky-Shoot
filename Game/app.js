ddfca35e6769ef85a63cf4020205aa54ec6bc37a
Tevin Crite <crite2004@gmail.com>
2024-04-15 02:08:32 -0400

    feat: Controls are working

--- Game/app.js
+++ Game/app.js
@@ -7,6 +7,9 @@ const enemyCount= document.querySelector('#enemies');
 const game = document.querySelector('#game');
 const ctx = game.getContext('2d');
 const testImg = document.getElementById('testImg');
+const testImg2 = document.getElementById('testImg2');
+const testShot = document.getElementById('test-shot');
+
 
 
 // ====================== Make Canvas Here =====================
@@ -14,12 +17,12 @@ game.setAttribute('height', getComputedStyle(game)['height']);
 game.setAttribute('width', getComputedStyle(game)['width']);
 
 window.addEventListener('DOMContentLoaded', function() {
-    enemies = new enemy(200, 400, testImg, 25, 50);
-    player = new Character(200, 100, testImg, 100, 200);
-
-     const runGame = setInterval(gameLoop, 100);
+    enemies = new enemy(50, 50, testImg2, 40, 40);
+    player = new Character(200, 225, testImg, 50, 50);
+    //shoot = new shot(player.x, player.y, testShot, 25, 50);
+    const runGame = setInterval(gameLoop, 100);
 });
-
+document.addEventListener('keydown', controls);
 
 // ====================== Make Player Here =====================
 let player;
@@ -52,6 +55,10 @@ class enemy {
         this.width = width;
         this.height = height;
         this.alive = true;
+
+        this.render = function() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
     }
 }
 
@@ -61,7 +68,20 @@ function controls(x) {
     console.log('movement :', x.key);
 
     if (x.key === 'ArrowLeft' || x.key === 'a') {
-        
+        player.x -= 5;
+    } else if (x.key === 'ArrowRight' || x.key === 'd') {
+        player.x += 5;
+    } else if (x.key === 'q') {
+        shoot = new shot(player.x, player.y, testShot, 25, 50);
+        setInterval(function() {
+            shoot.render();
+            shoot.y -= 1;
+        }, 1)
+        // while (shoot.y >= game.height) {
+        //     setInterval(function() {
+        //         shoot.y += 10;
+        //     }, 50);
+        // }
     }
 }
 
@@ -84,11 +104,29 @@ function makeEnemies() {
     }, 500);*/
 }
 
+let shoot;
+
+class shot {
+    constructor(x, y, image, width, height) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.alive = true;
+
+        this.render = function() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+}
 
 // ====================== Make Game Process Here ===============
 function gameLoop() {
     ctx.clearRect(0, 0, game.width, game.height);
     player.render();
+    enemies.render();
+   // shoot.render();
     setInterval
     while (timer < 0) {
         if (enemyCountNum < 0) {
